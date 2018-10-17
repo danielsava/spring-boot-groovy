afc01a8067f3c6440c7071be27f12b95dab20724
Daniel Sava <danielsavas@gmail.com>
2018-10-17 18:30:05 -0300

    Estrutura Básica

---
+++ src/main/groovy/br/com/savasoft/exemplos/springbootgroovy/SpringBootGroovyApplication.groovy
@@ -0,0 +1,13 @@
+package br.com.savasoft.exemplos.springbootgroovy
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringBootGroovyApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run SpringBootGroovyApplication, args
+	}
+
+}
